d3b482b4db9ab760552d5ed80000b395bea4a8bf
soojin <kimsj0012@naver.com>
2024-06-02 18:26:52 +0900

    mod 퀴즈 질문 선택지 셔플하기. fix 퀴즈 끝났을 때 빈 화면 뜨는 오류 해결

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const currentQuestionIndex = userAnswers.length;
+  const quizFinished = currentQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prev) => {
       return [...prev, selectedAnswer];
     });
   }
+
+  if (quizFinished) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[currentQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[currentQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[currentQuestionIndex].answers.map((ans) => (
+          {shuffledAnswers.map((ans) => (
             <li key={ans} className="answer">
               <button onClick={() => handleSelectAnswer(ans)}>{ans}</button>
             </li>
